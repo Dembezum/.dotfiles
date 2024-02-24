f4fd2d22d1582674a5515aed8c78a635b9b2435a
dembezum <dembezuuma@gmail.com>
2024-02-24 12:46:56 +0100

    add nix files

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+    description = "My first flake";
+
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager/master";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = { self, nixpkgs, home-manager, ... }: 
+        let 
+        system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    lib = nixpkgs.lib;
+    in {
+        nixosConfigurations = {
+            nixos = lib.nixosSystem {
+                inherit system;
+                modules = [ ./configuration.nix ];
+            };
+        };
+        homeConfigurations = {
+            nixtop = home-manager.lib.homeManagerConfiguration { 
+                inherit pkgs;
+                modules = [ ./home.nix ];
+            };
+        };
+    };
+}
