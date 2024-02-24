e7c389d6e5c94245c3aff0a056731a397b3e1ef4
dembezum <dembezuuma@gmail.com>
2024-02-24 14:38:59 +0100

    allow unfree in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,29 +1,44 @@
 {
-    description = "My first flake";
+  description = "My first flake";
 
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-unstable";
-        home-manager.url = "github:nix-community/home-manager/master";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: 
+    let 
+    system = "x86_64-linux";
+
+
+  Define an overlay that allows unfree packages
+    allowUnfreeOverlay = final: prev: {
+      nixpkgs.config = {
+        allowUnfree = true;
+      };
     };
 
-    outputs = { self, nixpkgs, home-manager, ... }: 
-        let 
-        system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    lib = nixpkgs.lib;
-    in {
-        nixosConfigurations = {
-            nixos = lib.nixosSystem {
-                inherit system;
-                modules = [ ./configuration.nix ];
-            };
-        };
-        homeConfigurations = {
-            nixtop = home-manager.lib.homeManagerConfiguration { 
-                inherit pkgs;
-                modules = [ ./home.nix ];
-            };
-        };
+# Apply the overlay to the package set
+  pkgs = import nixpkgs {
+    inherit system;
+    overlays = [ allowUnfreeOverlay ];
+  };
+
+  pkgs = nixpkgs.legacyPackages.${system};
+  lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
     };
+    homeConfigurations = {
+      nixtop = home-manager.lib.homeManagerConfiguration { 
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+  };
 }
