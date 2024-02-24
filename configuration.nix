806983043552429b6c1d0abd4c5bb9bf7a2f9f75
dembezum <dembezuuma@gmail.com>
2024-02-24 13:42:14 +0100

    edit config

--- configuration.nix
+++ configuration.nix
@@ -5,20 +5,20 @@
 { config, pkgs, ... }:
 
 {
-   imports =
-      [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ];
+  imports =
+    [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+      ./global/core.nix
+    ];
 
 # Enable flakes
-   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 # Bootloader.
-   boot.loader.systemd-boot.enable = true;
-   boot.loader.efi.canTouchEfiVariables = true;
-   boot.kernelModules = [ "evdi" ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-   networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 # Configure network proxy if necessary
@@ -26,144 +26,112 @@
 # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
 # Enable networking
-      networking.networkmanager.enable = true;
+    networking.networkmanager.enable = true;
 
 # Set your time zone.
-   time.timeZone = "Europe/Copenhagen";
+  time.timeZone = "Europe/Copenhagen";
 
 # Select internationalisation properties.
-   i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.defaultLocale = "en_DK.UTF-8";
 
-   i18n.extraLocaleSettings = {
-      LC_ADDRESS = "da_DK.UTF-8";
-      LC_IDENTIFICATION = "da_DK.UTF-8";
-      LC_MEASUREMENT = "da_DK.UTF-8";
-      LC_MONETARY = "da_DK.UTF-8";
-      LC_NAME = "da_DK.UTF-8";
-      LC_NUMERIC = "da_DK.UTF-8";
-      LC_PAPER = "da_DK.UTF-8";
-      LC_TELEPHONE = "da_DK.UTF-8";
-      LC_TIME = "da_DK.UTF-8";
-   };
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "da_DK.UTF-8";
+    LC_IDENTIFICATION = "da_DK.UTF-8";
+    LC_MEASUREMENT = "da_DK.UTF-8";
+    LC_MONETARY = "da_DK.UTF-8";
+    LC_NAME = "da_DK.UTF-8";
+    LC_NUMERIC = "da_DK.UTF-8";
+    LC_PAPER = "da_DK.UTF-8";
+    LC_TELEPHONE = "da_DK.UTF-8";
+    LC_TIME = "da_DK.UTF-8";
+  };
 
 # Enable the X11 windowing system.
-   services.xserver.enable = true;
+  services.xserver.enable = true;
 
-   services.xserver.videoDrivers = [ "displayLink" "modesetting" ];
-   services.xserver.displayManager.sessionCommands = '' ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0 '';
+  services.xserver.videoDrivers = [ "displayLink" "modesetting" ];
+  services.xserver.displayManager.sessionCommands = '' ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0 '';
 
 
 # Enable the GNOME Desktop Environment.
-   services.xserver.displayManager.gdm.enable = true;
-   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
 # Configure keymap in X11
-   services.xserver = {
-      layout = "dk";
-      xkbVariant = "";
-   };
+  services.xserver = {
+    layout = "dk";
+    xkbVariant = "";
+  };
 
 # Configure console keymap
-   console.keyMap = "dk-latin1";
+  console.keyMap = "dk-latin1";
 
 # Enable CUPS to print documents.
-   services.printing.enable = true;
+  services.printing.enable = true;
 
 # Enable sound with pipewire.
-   sound.enable = true;
-   hardware.pulseaudio.enable = false;
-   security.rtkit.enable = true;
-   services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
 # If you want to use JACK applications, uncomment this
 #jack.enable = true;
 
 # use the example session manager (no others are packaged yet so this is enabled by default,
 # no need to redefine it in your config for now)
 #media-session.enable = true;
-   };
+  };
 
 # Enable touchpad support (enabled default in most desktopManager).
 # services.xserver.libinput.enable = true;
 
 # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.nixtop = {
-      isNormalUser = true;
-      description = "nixtop";
-      extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [
-         git
-            firefox
-            neovim
-            eza
-            discord
-#  thunderbird
-      ];
-   };
+  users.users.nixtop = {
+    isNormalUser = true;
+    description = "nixtop";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      git
+        kitty
+        firefox
+        neovim
+        eza
+        discord
+        curl
+        wget
+        neovim
+        tmux
+        htop
+        btop
+        dstat
+        xclip
+        kitty
+    ];
+  };
 
 # Allow unfree packages
-   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-# List packages installed in system profile. To search, run:
-
-   environment.systemPackages = with pkgs; [
-         kitty
-         curl
-         wget
-         neovim
-         tmux
-         killall
-         usbutils
-         gnumake
-         displaylink
-         lm_sensors
-         home-manager
-         htop
-         btop
-         dstat
-         xclip
-         stdenv
-         glibc
-         gcc
-         binutils
-         clang
-         cmake
-         pkg-config
-         fontconfig
-         freetype
-
-         ];
-
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
-
-# List services that you want to enable:
-
-# Enable the OpenSSH daemon.
-#  services.openssh.enable = true;
+  services.openssh.enable = true;
 # SSH Server
-   services.openssh = {
-      enable = true;
-      settings.PasswordAuthentication = true;
+#   services.openssh = {
+#      enable = true;
+#      settings.PasswordAuthentication = true;
 #   settings.PermitRootLogin = "yes";
 
-   };
+};
 
 # Open ports in the firewall.
-   networking.firewall.allowedTCPPorts = [
-      22
-   ];
+networking.firewall.allowedTCPPorts = [
+  22
+];
 # networking.firewall.allowedUDPPorts = [ ... ];
 # Or disable the firewall altogether.
-   networking.firewall.enable = true;
+networking.firewall.enable = true;
 
 # This value determines the NixOS release from which the default
 # settings for stateful data, like file locations and database versions
@@ -171,5 +139,5 @@
 # this value at the release version of the first install of this system.
 # Before changing this value read the documentation for this option
 # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-   system.stateVersion = "unstable"; # Did you read the comment?
+system.stateVersion = "unstable"; # Did you read the comment?
 }
---
+++ global/packages.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ..}:
+
+{
+
+# System wide packages
+  environment.systemPackages = with pkgs; [
+      killall
+      usbutils
+      gnumake
+      lm_sensors
+      home-manager
+      stdenv
+      glibc
+      gcc
+      binutils
+      clang
+      cmake
+      pkg-config
+      fontconfig
+      freetype
+  ];
+}
