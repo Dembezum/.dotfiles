49f28a1df8e562956fa5714b028b03c3dd89c9d5
dembezum <dembezuuma@gmail.com>
2024-02-25 14:29:53 +0100

    add core.nix stuff

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -13,27 +13,31 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  boot.supportedFilesystems =
+    [ "btrfs" "cifs" "f2fs" "jfs" "ntfs" "reiserfs" "vfat" "xfs" ];
+
+
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c323aad0-c457-4011-973e-131207cfaefc";
-      fsType = "ext4";
-    };
+  { device = "/dev/disk/by-uuid/c323aad0-c457-4011-973e-131207cfaefc";
+    fsType = "ext4";
+  };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/A8FB-D45B";
-      fsType = "vfat";
-    };
+  { device = "/dev/disk/by-uuid/A8FB-D45B";
+    fsType = "vfat";
+  };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/71d22b87-f178-493d-afe6-5007c1e0a57a"; }
     ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
+# Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+# (the default) this is the recommended approach. When using systemd-networkd it's
+# still possible to use this option, but it's recommended to use it in conjunction
+# with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    networking.useDHCP = lib.mkDefault true;
+# networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- system/core.nix
+++ system/core.nix
@@ -1,17 +1,54 @@
 { config, pkgs, ... }:
 
 {
-  # Existing Home Manager configuration...
+# Existing Home Manager configuration...
   home.packages = with pkgs; [
     nodejs_21
-    neovim
-    kitty 
-    xclip 
-    tmux 
-    htop 
-    btop 
-    tree 
-    git 
-    eza 
+      neovim
+      kitty 
+      xclip 
+      tmux 
+      htop 
+      btop 
+      tree 
+      git 
+      eza 
   ];
+
+  environment.systemPackages with pkgs = [
+    w3m-nographics # needed for the manual anyway
+      testdisk # useful for repairing boot problems
+      ms-sys # for writing Microsoft boot sectors / MBRs
+      efibootmgr
+      efivar
+      parted
+      gptfdisk
+      ddrescue
+      ncdu
+      ccrypt
+      cryptsetup # needed for dm-crypt volumes
+
+# Some networking tools.
+      fuse
+      fuse3
+      sshfs-fuse
+      socat
+      screen
+      tcpdump
+
+# Hardware-related tools.
+      sdparm
+      hdparm
+      smartmontools # for diagnosing hard disks
+      pciutils
+      usbutils
+      nvme-cli
+
+# Some compression/archiver tools.
+      unzip
+      zip
+      ];
+  ];
+
 }
+
