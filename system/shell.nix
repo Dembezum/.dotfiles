cedfabafede2e540d902f9e7fe40b36aaa1068a6
dembezum <dembezuuma@gmail.com>
2024-02-25 15:07:55 +0100

    edit shell.nix

--- system/shell.nix
+++ system/shell.nix
@@ -1,30 +1,35 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixos-unstable> { } }:
+with pkgs;
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.gcc
-    pkgs.clang
-    pkgs.make
-    pkgs.cmake
-    pkgs.ninja
-    pkgs.python3
-    pkgs.nodejs
-    pkgs.openssl
-    pkgs.zlib
-    pkgs.gdb
-    pkgs.valgrind
-    pkgs.htop
-    pkgs.nginx
-    # Add any specific libraries for your project here
-    # For example, to add GTK development libraries:
-    # pkgs.gtk3.dev
-    # pkgs.glib.dev
-  ];
+    gcc
+      clang
+      make
+      cmake
+      binutils
+      ninja
+      python3
+      nodejs
+      openssl
+      zlib
+      gdb
+      libclang
+      lldb
+      valgrind
+      htop
+      nginx
+      glibc
+# Add any specific libraries for your project here
+# For example, to add GTK development libraries:
+# pkgs.gtk3.dev
+# pkgs.glib.dev
+      ];
 
-  # Setting some environment variables, if needed
+# Setting some environment variables, if needed
   shellHook = ''
     export EDITOR=neovim
     echo "Welcome to your development environment!"
-  '';
+    '';
 }
 
