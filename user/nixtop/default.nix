54d220625eac486e307e7f694baee2ed07d03739
dembezum <dembezuuma@gmail.com>
2024-02-27 20:53:58 +0100

    remove home.nix

--- user/nixtop/default.nix
+++ user/nixtop/default.nix
@@ -1,9 +1,38 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
+
 {
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+imports = [
   # Import everything
   imports = [
   ../base/media.nix
+  ./user/base/sh.nix
   ../modules/nvim
-./home.nix
+  ./home.nix
   ];
+];
+
+home.username = "nixtop";
+home.homeDirectory = "/home/nixtop";
+
+home.file = { };
+
+home.sessionVariables = {
+  EDITOR = "nvim";
+};
+
+programs.neovim.withNodeJs = true;
+
+programs.home-manager.enable = true;
+
+home.stateVersion = "23.11";
+}
+
+{
 }
