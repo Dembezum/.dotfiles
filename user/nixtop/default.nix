73326eaf767364aa64de0036c63d16b1dcd993ee
dembezum <dembezuuma@gmail.com>
2024-02-27 21:09:24 +0100

    fixed

--- user/nixtop/default.nix
+++ user/nixtop/default.nix
@@ -1,33 +1,11 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = (_: true);
-    };
-  };
-
 imports = [
   ../base/media.nix
   ../base/sh.nix
   ../modules/nvim/default.nix
   ./home.nix
 ];
-
-  home.username = "nixtop";
-  home.homeDirectory = "/home/nixtop";
-
-  home.file = { };
-
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-
-  programs.neovim.withNodeJs = true;
-
-  programs.home-manager.enable = true;
-
-  home.stateVersion = "23.11";
 }
 
